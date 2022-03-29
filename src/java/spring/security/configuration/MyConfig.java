496ff8a0b22ffb32405fa26fc26efe70e9f8a403
bazhukovsi <1q2w3e4R!@>
2022-03-29 16:07:21 +0300

    Initial commit

---
+++ src/java/spring/security/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package spring.security.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring.security")
+public class MyConfig {
+}
